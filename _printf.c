4363664dd4bf75f027eae64cd2c42a3e8dc0a33e
betsej <bejakpovi@gmail.com>
2022-11-21 10:16:28 +0100

    print

--- _printf.c
+++ _printf.c
@@ -3,10 +3,10 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- *  * _printf - Printf function
- *   * @format: format.
- *    * Return: Printed chars.
- *     */
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
 int i, printed = 0, printed_chars = 0;
@@ -20,32 +20,36 @@ return (-1);
 va_start(list, format);
 
 for (i = 0; format && format[i] != '\0'; i++)
-										{
-										if (format[i] != '%')								{
-										buffer[buff_ind++] = format[i];
-																				if (buff_ind == BUFF_SIZE)
-																				print_buffer(buffer, &buff_ind);
-																				/* write(1, &format[i], 1);*/
-																				printed_chars++;
-																				}
-										else
-										{
-																				print_buffer(buffer, &buff_ind);
-																				flags = get_flags(format, &i);
-																				width = get_width(format, &i, list);
-				
-																				precision = get_precision(format, &i, list);
-																				size = get_size(format, &i);
-																														++i;
-																																								printed = handle_print(format, &i, list, buffer,
-																																								flags, width, precision, size);																																									if (printed == -1)					return (-1);																																	printed_chars += printed;																																									}
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
 }
 
 print_buffer(buffer, &buff_ind);
 
-										va_end(list);
+va_end(list);
 
-										return (printed_chars);
+return (printed_chars);
 }
 
 /**
